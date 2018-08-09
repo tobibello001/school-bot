c26b141870ebeb1f8315c84e2a40dbd877df8a2c
Tobi Bello <bello.tobi@outlook.com>
2018-08-09 02:34:30 +0100

    to add mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+"use strict";
+
 const restify = require('restify');
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 const bot = require('./bot.js');
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Got error: ' + err.message);
+    })
 
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
