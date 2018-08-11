98912fad652607ed98f47230025de7b89ea210f1
Tobi Bello <bello.tobi@outlook.com>
2018-08-11 14:49:26 +0100

    index link field

--- models/posts.js
+++ models/posts.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
     title: String,
-    link: String,
+    link: {
+        type: String,
+        index: true
+    },
     updated: {
         type: Date,
         index: true
