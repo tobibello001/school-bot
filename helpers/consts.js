b92dace859ea71aa5626324b97dde508bc9dc937
Tobi Bello <bello.tobi@outlook.com>
2018-08-15 01:05:27 +0100

    to add more relevant message text and refactor

--- helpers/consts.js
+++ helpers/consts.js
@@ -1,13 +1,21 @@
+exports.PromptTexts = {
+    HELP: 'What would you like to know?'
+};
+
 exports.MessageTexts = {
+    CANCEL_HELP: 'Okay, nevermind.',
+    EXAMPLE_QUERY: [
+        'Show me news about inaugural lectures',
+    ],
     GREETINGS: ['Sup?', 'Hi there!', 'Hello!', 'Hey.'],
-    GREETING_RESPONSE: '%s What can I help you with?',
-    DEFAULT_RESPONSE: 'Oops, I didn\'t get that. What can I help you with?',
-    NO_POSTS : 'Oops, Sorry but I got nothing for now. Check back soon.'
-}
+    GREETING_RESPONSE: '%s Try something like: %s, or type "help".',
+    DEFAULT_RESPONSE: 'Oops, I didn\'t get that. Try something like: %s, or type "help".',
+    NO_POSTS: 'Oops, Sorry but I got nothing for now. Check back soon.',
+};
 
 exports.MenuTexts = ['Latest News'];
-// exports.Menus = {
-//     help: [
-//         { title: 'Latest News', dialogId: 'getLastestNews' },
-//     ]
-// };
+exports.Menus = {
+    HELP_MENU: [
+        { title: 'Latest News', dialogId: 'getLastestInfo' },
+    ]
+};
