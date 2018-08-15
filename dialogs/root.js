195e6d195b083fc9dd417b0d8e301df68e7031d0
Tobi Bello <bello.tobi@outlook.com>
2018-08-15 01:10:06 +0100

    to refactor default dialog to simple text message

--- dialogs/root.js
+++ dialogs/root.js
@@ -4,25 +4,22 @@ const {
     IntentDialog,
 } = require('botbuilder');
 
-const { MessageTexts, MenuTexts } = require('../helpers/consts');
+const { MessageTexts } = require('../helpers/consts');
 
 const utils = require('../helpers/utils');
 
 module.exports = new IntentDialog({ recognizers: [utils.witRecognizer] })
     .matches('latest_info', DialogAction.beginDialog('getLatestInfo'))
+    .matches('find_info', DialogAction.beginDialog('findInfo'))
     .onDefault((session, args) => {
-        const messageText = session.message.text;
+        // const messageText = session.message.text;
         const { entities } = args;
 
         // Extract all the useful entities.
         const greeting = EntityRecognizer.findEntity(entities, 'greetings');
-        let card = new ThumbnailCard(session)
-            .buttons(MenuTexts.map(menuText => CardAction.imBack(session, menuText, menuText)));
-        let message = new Message(session).addAttachment(card);
-        if (greeting)
-            message.text(MessageTexts.GREETING_RESPONSE, utils.getRandomGreeting());
-        else
-            message.text(MessageTexts.DEFAULT_RESPONSE)
 
-        session.endDialog(message);
+        if (greeting)
+            session.endDialog(MessageTexts.GREETING_RESPONSE, utils.getRandomGreeting(), utils.getRandomQuery());
+        else
+            session.endDialog(MessageTexts.DEFAULT_RESPONSE, utils.getRandomQuery());
     });
