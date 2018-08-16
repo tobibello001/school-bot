ee52cbd1702aa684c5c09308ae745de0c9c33089
Tobi Bello <bello.tobi@outlook.com>
2018-08-16 13:49:16 +0100

    to refactor

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const restify = require('restify');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const restify = require('restify')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const bot = require('./bot.js');
+const bot = require('./bot.js')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     .then(() => {
-        console.log('Connected to database');
+        console.log('Connected to database')
     })
     .catch((err) => {
-        console.error('Got error connecting to database: ' + err.message);
-    });
+        console.error('Got error connecting to database: ' + err.message)
+    })
 
-const server = restify.createServer();
-server.post('/api/messages', bot.connector('*').listen());
+const server = restify.createServer()
+server.post('/api/messages', bot.connector('*').listen())
 server.listen(process.env.PORT, () => {
-    console.log(`${server.name} listening to ${server.url}`);
-});
+    console.log(`${server.name} listening to ${server.url}`)
+})
--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const { Middleware, UniversalBot, ChatConnector } = require('botbuilder');
-const mongoose = require('mongoose');
-const { MongoBotStorage } = require('botbuilder-storage');
+const { Middleware, UniversalBot, ChatConnector } = require('botbuilder')
+const mongoose = require('mongoose')
+const { MongoBotStorage } = require('botbuilder-storage')
 
-const dialogs = require('./dialogs');
+const dialogs = require('./dialogs')
 
 const bot = new UniversalBot(
     new ChatConnector({
@@ -10,11 +10,11 @@ const bot = new UniversalBot(
         appPassword: process.env.MICROSOFT_APP_PASSWORD
     }),
     dialogs.root.waterfall
-);
+)
 
-bot.use(Middleware.sendTyping());
+bot.use(Middleware.sendTyping())
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 connection.once('open', () => {
     const adapter = new MongoBotStorage(
         connection.db,
@@ -26,13 +26,13 @@ connection.once('open', () => {
                 privateConversationData: 3600 * 24 * 7
             }
         }
-    );
-    bot.set('storage', adapter);
-});
+    )
+    bot.set('storage', adapter)
+})
 
-bot.dialog('/', dialogs.root);
-bot.dialog(dialogs.getLatestInfo.id, dialogs.getLatestInfo.waterfall);
-bot.dialog(dialogs.findInfo.id, dialogs.findInfo.waterfall);
-bot.dialog(dialogs.help.id, dialogs.help.waterfall).triggerAction({ matches: dialogs.help.name});
+bot.dialog('/', dialogs.root)
+bot.dialog(dialogs.getLatestInfo.id, dialogs.getLatestInfo.waterfall)
+bot.dialog(dialogs.findInfo.id, dialogs.findInfo.waterfall)
+bot.dialog(dialogs.help.id, dialogs.help.waterfall).triggerAction({ matches: dialogs.help.name})
 
-module.exports = bot;
+module.exports = bot
