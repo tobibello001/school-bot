e21d6e35a4be31cf05b42bfff33a642efcd8ada6
Tobi Bello <bello.tobi@outlook.com>
2018-08-19 15:27:32 +0100

    refactor: delete findInfo file and clean up code

--- dialogs/findInfo.js
+++
@@ -1,9 +0,0 @@
-const { MessageTexts } = require('../helpers/consts');
-
-module.exports = {
-    id: 'findInfo',
-    name: undefined,
-    waterfall: (session) => {
-        session.endDialog(MessageTexts.NO_POSTS);
-    }
-};
