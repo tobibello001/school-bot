633c61ec4fdbf2fa6b2bde275d212aeb59eea39c
Tobi Bello <bello.tobi@outlook.com>
2018-08-19 15:31:42 +0100

    chore: to add route to increment post click

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const restify = require('restify')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const PostModel = require('./models/posts')
 const bot = require('./bot.js')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, autoIndex: false })
@@ -13,7 +14,22 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, autoIndex: fa
     })
 
 const server = restify.createServer()
+server.use(restify.plugins.queryParser())
 server.post('/api/messages', bot.connector('*').listen())
+server.get('/link', async (req, res, next) => {
+    let { url, ref } = req.query
+    res.redirect(301, url, next)
+    PostModel.findById(ref, (err, post) => {
+        if (err) return console.error(err)
+        post.clicks += 1
+        post.lastClicked = new Date()
+        post.save((err) => {
+            if (err) return console.error(err)
+        })
+    })
+    
+})
+
 server.listen(process.env.PORT, () => {
     console.log(`${server.name} listening to ${server.url}`)
 })
